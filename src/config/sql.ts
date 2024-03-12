adc9482f2095e663ad05d482a4fd9dd7c1dc9fc0
mhhong <2550593431@qq.com>
2024-03-12 17:58:56 +0800

    feat: 删除敏感信息

--- src/config/sql.ts
+++ src/config/sql.ts
@@ -1,7 +1,7 @@
 const sqlConfig = {
     host: 'localhost',
     user: 'root', // 数据库用户名
-    password: 'hmh6861860', // 数据库密码
+    password: 'xxxx', // 数据库密码
     database: 'blog_app', // 数据库名
 };
 
