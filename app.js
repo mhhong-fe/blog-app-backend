259b85a82192e8b01b20d6c1215f404f029cbe31
mhhong <hongminghui@baijia.com>
2024-08-14 17:34:35 +0800

    feat: 增加搜索接口

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = 4000;
+
+// 使用 CORS 中间件
+app.use(cors());
+
+// 中间件，解析 JSON 请求体
+app.use(express.json());
 
 app.get("/blogInfo", (req, res) => {
   const userInfo = {
     message: "Hello Blog",
   };
   res.json(userInfo);
+});
+
+app.post("/blogList", (req, res) => {
+  const { keyword } = req.body;
+  res.json({ code: 200, data: [], msg: "success" });
 });
 
 app.listen(4000, () => {
