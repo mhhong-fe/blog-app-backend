521196a2e3078d3adc46d053414bba8bed128a63
mhhong-fe <2550593431@qq.com>
2024-10-20 12:45:31 +0800

    feat: 去掉无关信息

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,11 +3,11 @@ const mysql = require("mysql2/promise");
 // 需要把这里换成真实的服务器信息
 // 设置MySQL连接池
 const pool = mysql.createPool({
-    host: "114.55.133.112",
+    host: "xxxxxx",
     port: 3306,
     user: "root",
-    password: "@Hmh6861860@",
-    database: "blog_app",
+    password: "xxxxxx",
+    database: "xxxxx",
     waitForConnections: true,
     connectionLimit: 10, // 连接池中允许的最大连接数
     queueLimit: 0, // 队列中允许的最大请求数，0表示无限制
